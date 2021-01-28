be9dab2d68325b97bba94131010e8585d12baafe
Allison Chen <allison.chen@oracle.com>
2021-01-28 10:19:41 -0600

    created home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import logo from './assets/nourish_logo.png';
 import './App.css';
+import './css/global.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="body">
+      {/* <header> */}
+      <header className="header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          nourish
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          Click Me
+        </button>
       </header>
     </div>
   );
